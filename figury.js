1296d30ca9612b78d4c1764e37df10b322c276e4
marskib <tata.marcina@autyzmsoft.pl>
2020-02-24 22:09:46 +0100

    cd... karekta marginesu

--- figury.js
+++ figury.js
@@ -13,7 +13,7 @@ class TMKanwa {
         this.kanwa.style.width  = dx+"%";
         this.kanwa.style.height = dy+"%";        
     }
-    
+
     zrobOutline() {
         this.ctx.lineWidth = 4;
         this.ctx.strokeStyle = '#000000';
@@ -34,38 +34,59 @@ class TMKanwa {
         this.zrobOutline();
     }
 
-}   //koniec TMKanwa
+    rysujKwadrat(x0,y0,r) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(x0-r/2,y0-r/2);
+        this.ctx.lineTo(x0+r/2, y0-r/2);    
+        this.ctx.lineTo(x0+r/2, y0+r/2);    
+        this.ctx.lineTo(x0-r/2, y0+r/2);    
+        this.ctx.closePath();
+        //the outline
+        this.zrobOutline();        
+    }
+
+    rysujTrojkat(x0,y0,r) {
+        this.ctx.beginPath();
+        this.ctx.moveTo(x0-r/2,y0-r/2);
+        this.ctx.lineTo(x0+r/2, y0-r/2);    
+        this.ctx.lineTo(x0+r/2, y0+r/2);    
+        // this.ctx.lineTo(x0-r/2, y0+r/2);    
+        this.ctx.closePath();
+        //the outline
+        this.zrobOutline();        
+    }
+
+}   //koniec klasy TMKanwa
 
 
-
-//
-//  let probna = new TMKanwa(document.querySelector('canvas').getContext("2d"), 300,300);
-//probna.rysujLinie(10,10,40,40);
-
-
-//Tablica 'kanwy' zostaje 'pobrana' z dokumentu:
+//Tablica 'kanwy' zostaje 'pobrana' z dokumentu, zeby potem mozna ja bylo wstawic do kanwyObj[]:
 let kanwy = Array.from(document.querySelectorAll('canvas'));
 
-
+//Zm. glob. -> w jakim stanie sa kanwy (duza/male):
 let stanDuza = false;
+
 let handleKlikOnKanwa = function (event) {
-/* pOKAZANIE DUZEJ CANWY Z POJEDYNCZA DUZA FIGURA */
-    //event.target.getContext("2d").fillRect(10,10,100,100);
-    //'Znikniecie' wszystkich innych niz kliknieta:
-
-
-    console.log(stanDuza)
+/* POKAZANIE DUZEJ CANWY Z POJEDYNCZA DUZA FIGURA */
+    var indxKliknietej = -1;   
 
     if (!stanDuza) {
+        //Chowanie wszystkich, pokazanie w powiekszeniu kanwy kliknietej:
         for (let i=0; i<kanwyObj.length; i++) {
             if (kanwyObj[i].kanwa!==event.target) {
                 kanwyObj[i].kanwa.style.display = "none";
             }
+            else 
+              //przechwycenie indeksu kliknietej:
+              indxKliknietej = i;
         }
-        event.target.style.width  = "100%";
-        event.target.style.height = "100%";
+        //Powiekszanie kliknietej:
+        event.target.style.width  = "97%";
+        event.target.style.height = "97%";
+        //Korygowanie marginesu kliknietej kanwy - doswiadczalnie w zaleznosci od wiersza:
+        event.target.style.marginTop = korektaMarginTop(indxKliknietej) ;
         stanDuza = true;
     }
+    //Powrot do 9-ciu kmalych kanw:
     else {
         event.target.style.width  = "32%";
         event.target.style.height = "32%";
@@ -76,6 +97,13 @@ let handleKlikOnKanwa = function (event) {
     }
 }
 
+function korektaMarginTop(idx) {
+//Zwraca wartosc o jaka nalezy skorygowac margines kanwy o indeksie idx,
+//(invoked only if the canvas is enlarged)
+    var row = 0; //ktory to wiersz -> 0,1,2
+    row = Math.trunc(idx/3);
+    return -(row*18)+'px';
+}
 
 
 //Tablica obiektów TMKanwa:
@@ -88,9 +116,18 @@ kanwyObj.forEach(elem=>elem.kanwa.onclick=handleKlikOnKanwa);
 
 
 //rysowanie linii na kazdej:
-//kanwyObj.forEach((elem,ind)=>elem.rysujLinie(10,10,150,20*ind));
+// kanwyObj.forEach((elem,ind)=>elem.rysujLinie(10,10,150,20*ind));
+
+//rysowanie kola na kazdej:
 kanwyObj.forEach((elem,ind)=>elem.rysujOkrag(100,100,10*(ind+1)));
 
+
+//rysowanie kwadratu na kazdej:
+// kanwyObj.forEach((elem,ind)=>elem.rysujKwadrat(100,100,10*(ind+1)));
+
+
+//rysowanie trojkata na kazdej:
+kanwyObj.forEach((elem,ind)=>elem.rysujTrojkat(100,100,10*(ind+1)));
 
 
 
